5a7d5021c8e36f960f6dbad4a25edeb6f345c029
Abhirami kannan <85607268+Abhiramikannan@users.noreply.github.com>
2025-06-30 10:53:39 +0530

    Update 01_main.tf

--- exam-practice/terraform/01_main.tf
+++ exam-practice/terraform/01_main.tf
@@ -1,3 +1,115 @@
--- create the vm using terrform
+aim: create a vm using terraform
 steps involved:
-    
+    rg,vnet,subnet,publicip,nsg,association,nic,vm
+got errorslike:
+    subscription id required (add variable in pipeline subscription and give the subscription id ,and give env and give)
+    terraform dont have permssions so dont give resource group
+
+pipeline:
+
+        repo create ,add 
+        create agent pool   
+        create service connection ,dont give resource group,else->error -terraform doesnt have any permssion
+        create pipeline-install az client,terraform commands,added the subscription id in variables
+        env:
+        
+          ARM_SUBSCRIPTION_ID: $(subscription)
+
+
+code:
+====================================
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "azurerm"
+      version = "4.24.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  location   = "eastus2"
+  name       = "abhi-rg2"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  address_space                  = ["10.0.0.0/16"]
+  location                       = azurerm_resource_group.rg.location
+  name                           = "abhi-vm-vnet"
+  resource_group_name            = azurerm_resource_group.rg.name
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  address_prefixes                              = ["10.0.0.0/24"]
+  name                                          = "abhi-subnet"
+  resource_group_name                           = azurerm_resource_group.rg.name
+  virtual_network_name                          = "abhi-vm-vnet"
+  depends_on = [
+    azurerm_virtual_network.vnet
+  ]
+}
+
+
+resource "azurerm_public_ip" "res-6" {
+  allocation_method       = "Static"
+  location                = azurerm_resource_group.rg.location
+  name                    = "abhi-vm-ip"
+  resource_group_name     = azurerm_resource_group.rg.name
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  location            = azurerm_resource_group.rg.location
+  name                = "abhi-vm-nsg"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "res-3" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+
+resource "azurerm_network_interface" "nic" {
+  location                       = azurerm_resource_group.rg.location
+  name                           = "abhi-nic"
+  resource_group_name            = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                                               = "ipconfig1"
+    private_ip_address_allocation                      = "Dynamic"
+    public_ip_address_id                               = azurerm_public_ip.res-6.id
+    subnet_id                                          = azurerm_subnet.subnet.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "res-1" {
+  admin_password                                         = "Abhi@12345678" # Masked sensitive attribute
+  admin_username                                         = "abhi"
+  computer_name                                          = "abhi-vm"
+  location                                               = azurerm_resource_group.rg.location
+  name                                                   = "abhi-vm1"
+  network_interface_ids                                  = [azurerm_network_interface.nic.id]
+  resource_group_name                                    = "abhi-rg"
+  size                                                   = "Standard_B2s"
+  disable_password_authentication                        = false
+  os_disk {
+    caching                          = "ReadWrite"
+    name                             = "abhi-vm_OsDisk"
+    storage_account_type             = "Premium_LRS"
+  }
+  source_image_reference {
+    offer     = "ubuntu-24_04-lts"
+    publisher = "canonical"
+    sku       = "server"
+    version   = "latest"
+  }
+}
+
